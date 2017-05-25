2fa2d0a91afed7589bf897b476be2198822ab7e6
PedroDeAlbuquerque <pedro.de.albuquerque@hotmail.com>
2017-05-25 17:53:56 -0300

    user mailer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/sign_in'
   post 'users/sign_up'
   get 'users/sign_out'
+  get 'users/send_support_email'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
