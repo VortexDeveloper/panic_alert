7aa576a5a3c0f91ca1c75d73607f82fa46a7c759
Thiago Ururay <taraujo@id.uff.br>
2017-06-02 15:09:08 -0300

    scaping nullable device tokens

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,8 +2,12 @@ class NotificationsController < ApplicationController
   before_action :authenticate!
 
   def create
+    device_tokens = current_user.accepted_dependent_requests.map do |u|
+      u.device_token if u.device_token.present?
+    end
+    byebug
     notification = IonicNotification::Notification.new(
-      tokens: current_user.accepted_dependent_requests.map(&:device_token),
+      tokens: device_tokens,
       message: notifications_params[:message],
       title: "Alerta de Pânico"
     )
