9300f81cc3a3c26a526afd20cdea385e78d19f39
Thiago Ururay <taraujo@id.uff.br>
2017-06-02 16:36:14 -0300

    testing payloads with content_available 4

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,12 +9,12 @@ class NotificationsController < ApplicationController
       tokens: device_tokens,
       message: "Está pedindo sua ajuda",
       title: "Pânico do Alerta | #{current_user.name}",
-      "content-available": "1",
       payload: {
         data: {
           title: "Test Notification",
           body: "This offer expires at 11:30 or whatever",
           notId: 10,
+          content_available: true,
           surveyID: "ewtawgreg-gragrag-rgarhthgbad"
         }
       }
